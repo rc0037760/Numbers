db6a88cec1f36b131fa4799ac371b5dee38d97a0
Max Burton <rc0037760@student.riversidecollege.ac.uk>
2019-09-17 12:46:05 +0100

    Number v3

--- Numbers/Form1.cs
+++ Numbers/Form1.cs
@@ -24,7 +24,37 @@ namespace Numbers
             richTextBox1.AppendText(c.ToString("0.000000000000000E0") + "\n");
             richTextBox1.AppendText(c.ToString("0") + "\n");
             richTextBox1.AppendText(c.ToString("N0") + "\n");
+
+            string st = "three";
+            try
+            {
+                int it = int.Parse(st);
+            }
+            catch
+            {
+                MessageBox.Show(st + " is not an integer");
+            }
+            string s2 = "3.2";
+            try
+            {
+                int it = int.Parse(s2);
+            }
+            catch
+            {
+                MessageBox.Show(s2 + " is not an integer");
+                int i2 = 0;
+            }
+            string s3 = "pi";
+            try
+            {
+                double d2 = double.Parse(s3);
+            }
+            catch
+            {
+                double d2 = 0;
+            }
         }
+
 
         private void RichTextBox1_TextChanged(object sender, EventArgs e)
         {
